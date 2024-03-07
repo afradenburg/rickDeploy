154b326eff5773b149da0bd87f706640bedf721c
afradenburg <andres.burg@hotmail.com>
2024-03-07 15:41:28 -0500

    add create user

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -1,4 +1,11 @@
-import { RESET, ADD_FAV, REMOVE_FAV, FILTER, ORDER, CREATEUSER } from "./actionsTypes";
+import {
+  RESET,
+  ADD_FAV,
+  REMOVE_FAV,
+  FILTER,
+  ORDER,
+  CREATEUSER,
+} from "./actionsTypes";
 import axios from "axios";
 
 export const addFav = (character) => {
@@ -6,50 +13,47 @@ export const addFav = (character) => {
   return async (dispatch) => {
     try {
       const { data } = await axios.post(endpoint, character);
-      if(data.length> 0)
-      dispatch({ type: ADD_FAV,
-    payload: data});
-      console.log(data)
+      if (data.length > 0) dispatch({ type: ADD_FAV, payload: data });
+      console.log(data);
     } catch (error) {
-      console.log("Error al añadir a favorito", error )
-  }
+      console.log("Error al añadir a favorito", error);
+    }
   };
 };
-export const createUser = ( newUser ) => {
+export const createUser = (newUser) => {
   const endpoint = "/newuser";
-  return async (dispatch) =>{
-  try {
-    const data = await axios.post(endpoint, newUser)
-    console.log(data)
-    if (data.status === 201) {
-      alert("usuario creado correctamente")
-    return dispatch({
-      type: CREATEUSER,
-      payload: data
-  })
+  return async (dispatch) => {
+    try {
+      const data = await axios.post(endpoint, newUser);
+      console.log(data);
+      if (data.status === 201) {
+        alert("usuario creado correctamente");
+        return dispatch({
+          type: CREATEUSER,
+          payload: data,
+        });
+      }
+    } catch (error) {
+      alert("este correo ya esta en uso");
     }
-  } catch (error) {
-    alert("este correo ya esta en uso")
-  }
-}
-}
+  };
+};
 
 export const removeFav = (id) => {
   const endpoint = "/fav/" + id;
   return async (dispatch) => {
     try {
-      const {data} = await axios.delete(endpoint)
-      if(data.id > 800 ) throw Error(" No hay favoritos con ese id")
+      const { data } = await axios.delete(endpoint);
+      if (data.id > 800) throw Error(" No hay favoritos con ese id");
       return dispatch({
         type: REMOVE_FAV,
         payload: data,
       });
     } catch (error) {
-      console.log("Error al eliminar un favorito", error )
-    } 
+      console.log("Error al eliminar un favorito", error);
     }
   };
-
+};
 
 export const filterCards = (gender) => {
   return {
